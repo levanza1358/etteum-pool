b7aea63e4ab039d5bed145b113e315cf2a371ac3
levanza1358 <kiki.manda16@gmail.com>
2026-06-16 12:40:54 +0700

    chore: remove non-essential tests and notes

--- test/manual/check-tables.ts
+++
@@ -1,4 +0,0 @@
-import { Database } from "bun:sqlite";
-const db = new Database("./data/poolprox3.db");
-const tables = db.query("SELECT name FROM sqlite_master WHERE type='table' ORDER BY name").all();
-console.log("Existing tables:", tables);
